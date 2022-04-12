c3cf8eca748be0b7fd4827b151919f160262a6ed
Andrew Reilley <reilleya@mit.edu>
2022-04-12 00:28:38 -0400

    Add barebones gcode plotter

--- src/cli-entry.ts
+++ src/cli-entry.ts
@@ -1,7 +1,8 @@
 import { MarlinPort } from './marlin-port';
 import { planWind } from './planner';
+import { plotGCode } from './plot';
 import { hideBin } from 'yargs/helpers';
-import { promises as fs } from "fs";
+import { promises as fs, createWriteStream } from 'fs';
 
 // Looks like using yargs most any other way is kind of broken
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
@@ -45,6 +46,25 @@ require('yargs').command({
         const windCommands = planWind(windDefinition);
         await fs.writeFile(argv.output, windCommands.join('\n'));
         console.log(`Wrote ${windCommands.length} commands to "${argv.output}"`);
+    }
+})
+.command({
+    command: 'plot <file>',
+    describe: 'Visualize the contents of a gcode file',
+    builder: {
+        output: {
+            alias: 'o',
+            describe: 'PNG file to output to',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    async handler(argv: Record<string, string>) {
+        const fileContents = await fs.readFile(argv.file, "binary");
+        const stream = plotGCode(fileContents.split('\n'));
+        const outputFile = createWriteStream(argv.output);
+        stream.pipe(outputFile);
+        outputFile.on('finish', () => console.log(`The PNG file was created at ${argv.output}`));
     }
 })
 .help()
---
+++ src/plot.ts
@@ -0,0 +1,79 @@
+import { createCanvas, loadImage } from 'canvas';
+import { Readable } from 'stream';
+
+// TODO: move this
+interface ICoordinate2D {
+    x: number;
+    y: number;
+}
+
+// Turn two machine coordinate endpoints into a set of screen-space line segments
+function generateCoordinates(start: ICoordinate2D, end: ICoordinate2D): ICoordinate2D[][] {
+    // Are both endpoints in the same modulo? If so, mod360 and return
+    if (Math.floor(start.y / 360) === Math.floor(end.y / 360)) {
+        return [[{x: start.x, y: start.y % 360}, {x: end.x, y: end.y % 360}]];
+    }
+
+    // If not, calc the intersection point between the ray and the relevant edge
+    const slope = (end.y - start.y) / (end.x - start.x);
+
+    let currentSegmentEndY = 0;
+    let nextSegmentStartY = 360 * Math.floor(start.y / 360);
+    let nextSegmentStartYAdj = -0.001; // TODO: refactor this to not care if the segment ends on 360N
+
+    if (end.y > start.y) {
+        currentSegmentEndY = 360;
+        nextSegmentStartY = 360 * Math.ceil(start.y / 360);
+        nextSegmentStartYAdj = 0.001;
+    }
+
+    const currentSegmentEndX = start.x + ((nextSegmentStartY - start.y) * (1/slope));
+
+    return [[{x: start.x, y: start.y % 360}, {x: currentSegmentEndX, y: currentSegmentEndY}]].concat(
+        generateCoordinates({x: currentSegmentEndX, y: nextSegmentStartY + nextSegmentStartYAdj}, end)
+    )    
+}
+
+export function plotGCode(gcode: string[]): Readable {
+    const canvas = createCanvas(500, 360);
+    const ctx = canvas.getContext('2d');
+
+    let xCoord = 0;
+    let yCoord = 0;
+
+    ctx.strokeStyle = 'rgba(0,0,0,1)';
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    for (const line of gcode) {
+        const lineParts = line.split(' ');
+        if (lineParts[0] != 'G0') {
+            console.log(`Unknown gcode line: '${line}', skipping`)
+            continue;
+        }
+
+        let nextXCoord = xCoord;
+        let nextYCoord = yCoord;
+        for (const coordinate of lineParts.slice(1)) {
+            if (coordinate[0] === 'X') {
+                nextXCoord = Number.parseFloat(coordinate.slice(1));
+            }
+            if (coordinate[0] === 'Y') {
+                nextYCoord = -Number.parseFloat(coordinate.slice(1));
+            }
+        }
+
+        for (const segment of generateCoordinates({x: xCoord, y: yCoord}, {x: nextXCoord, y: nextYCoord})) {
+            ctx.beginPath();
+            for (const point of segment) {
+                ctx.lineTo(point.x, point.y);
+            }
+            ctx.stroke();
+        }
+        
+        xCoord = nextXCoord;
+        yCoord = nextYCoord;
+    }
+
+    return canvas.createPNGStream();
+};
